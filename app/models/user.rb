368cf6636f3773a6a97746bea0363a879c213463
Konrad Dziewit <konrad.dziewit@onet.pl>
2023-04-26 15:58:42 +0200

    :L

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,9 @@
 class User < ApplicationRecord
+  has_secure_password
   validates :email, presence: true, format: { with: URI::MailTo::EMAIL_REGEXP}
   validates :name, presence: true
   validates :surname, presence: true
+  validates :password_digest
   validate :valid_email?
 
   def valid_email?
--- db/schema.rb
+++ db/schema.rb
@@ -10,14 +10,22 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2023_04_22_121516) do
+ActiveRecord::Schema.define(version: 2023_04_26_120309) do
 
   create_table "users", force: :cascade do |t|
-    t.name "name"
-    t.surname "surname"
-    t.email "email"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "username"
+    t.string "name"
+    t.string "surname"
+    t.string "email"
+    t.string "encrypted_password", default: "", null: false
+    t.string "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.index ["email"], name: "index_users_on_email", unique: true
+    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
+    t.index ["username"], name: "index_users_on_username", unique: true
   end
 
 end
